b7d64537b2f899f7d4b159d4d30146edb58f68ba
bhavesh-kransan <bhaveshg1357@gmail.com>
2026-07-19 17:02:31 +0530

    Add connection_request to Notification schema enum

--- Backend/models/notification.model.js
+++ Backend/models/notification.model.js
@@ -14,7 +14,7 @@ const notificationSchema = new mongoose.Schema(
     },
     type: {
       type: String,
-      enum: ["like", "comment", "follow", "message"],
+      enum: ["like", "comment", "follow", "message", "connection_request"],
       required: true,
     },
     post: {
